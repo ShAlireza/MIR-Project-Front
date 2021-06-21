d68aaca5217c80250b274f8b5f3c266f8acd0773
saman-re <saman.rezapoor1379@yahoo.com>
2021-06-21 19:53:43 +0430

    third page done

--- src/views/third.vue
+++ src/views/third.vue
@@ -1,5 +1,39 @@
 <template>
   <v-container>
-    this is third page
+      <v-file-input
+          outlined
+          dense
+          rounded
+          label="Information File"
+          accept=".json"
+          show-size
+          v-model="infoFile"
+          append-icon="mdi-paperclip"
+          prepend-icon=""
+        />
+    <v-text-field
+      label="Number of Top Author"
+      outlined
+      dense
+      rounded
+      autocomplete="off"
+      v-model="topAuthor"
+      type="number"
+    ></v-text-field>
+
+    <v-btn color="primary" rounded>submit</v-btn>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      infoFile:null,
+      topAuthor: "",
+    };
+  },
+  methods: {
+  },
+};
+</script>
